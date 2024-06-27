b0acf51324fdbebeeee15d5f5b53198a2509aa51
annahosanna <annahosanna@gmail.com>
2024-06-27 17:40:36 -0400

    Make changes to Dockerfile

--- Gitlab-Container-Client/deploy.sh
+++ Gitlab-Container-Client/deploy.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+
+# Deploy ECS Fargate cloudformation (or whatever)
 aws cloudformation deploy --template-file template.yaml --stack-name my-fargate-stack --capabilities CAPABILITY_NAMED_IAM
--- Gitlab-Container-Client/entrypoint.sh
+++ Gitlab-Container-Client/entrypoint.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/bash
 
 # Import SSH key from /dev/1/environ
@@ -22,3 +23,45 @@ exec proot \
 
 # Infinite sleep
 sleep infinity
+=======
+#!/bin/sh
+###################################
+# Import SSH key from injected environment variable
+# eval $(cat /dev/1/environ/SSH_KEY | awk -F "=" '{print "export "$1"="$2}')
+export $(strings /proc/1/environ | grep SSH_KEY)
+
+# Import the GitLab client SSH key at runtime
+# if [ -f /dev/1/environ/SSH_KEY ]; then
+#     SSH_KEY=$(cat /dev/1/environ/SSH_KEY)
+#     SSH_KEY_VALUE=${SSH_KEY#*=}
+#     mkdir -p /root/.ssh
+#     echo "$SSH_KEY_VALUE" > /root/.ssh/id_rsa
+#     chmod 600 /root/.ssh/id_rsa
+# else
+#     echo "SSH key not found at /dev/1/environ/SSH_KEY"
+#     exit 1
+# fi
+
+# SSH_KEY=$(echo $SSH_KEY | cut -d '=' -f 2)
+
+# Need to check if env var is defined
+
+# Setup the SSH key
+mkdir -p /home/gitlab-ce/.ssh
+echo "$SSH_KEY" > /home/gitlab-ce/.ssh/id_rsa
+chmod 600 /gitlab-ce/.ssh/id_rsa
+chown gitlab-ce:gitlab-ce /home/gitlab-ce/.ssh/id_rsa
+# need to set up correct home directory ownership
+
+# Run proot and execute kaniko
+# proot will need to be run in the pipeline
+# EXCLUDES="-b /etc/mtab:/etc/mtab -b /etc/resolv.conf:/etc/resolv.conf -b /etc/hosts:/etc/hosts -b /etc -b /sys -b /proc"
+# cd /kaniko
+#proot -b /etc/mtab -b /etc/resolv.conf -b /etc/hosts -b /etc -b /sys -b /proc -- /kaniko/executor "$@"
+
+# Keep the container running
+# This is the lowest overhead infinite loop
+# sleep infinity
+/usr/bin/sshd
+# Execute gitlab-ce
+>>>>>>> 0f251fa (Make changes to Dockerfile)
