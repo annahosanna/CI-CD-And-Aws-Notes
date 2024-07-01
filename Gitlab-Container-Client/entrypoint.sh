ce00e0071ad30c1fbbe74eb13025a3d1c2c3a90c
annahosanna <Anna.Hosanna@gmail.com>
2024-06-30 20:28:48 -0400

    Update entrypoint.sh

--- Gitlab-Container-Client/entrypoint.sh
+++ Gitlab-Container-Client/entrypoint.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-# Import SSH key
-eval $(cat /dev/1/environ/SSH_KEY | awk -F "=" '{print "export "$1"="$2}')
+# Import SSH key from /dev/1/environ
+SSH_KEY=$(strings /dev/1/environ | grep SSH_KEY | cut -d'=' -f2)
+if [ -n "$SSH_KEY" ]; then
+    echo "$SSH_KEY" > /home/gitlab-ce/.ssh/id_rsa
+    chmod 600 /home/gitlab-ce/.ssh/id_rsa
+    chown gitlab-ce:gitlab-ce /home/gitlab-ce/.ssh/id_rsa
+fi
 
-# Run proot and execute kaniko
-proot -b /etc/mtab -b /etc/resolv.conf -b /etc/hosts -b /etc -b /sys -b /proc -- /kaniko/executor "$@"
+# Execute proot with exclusions and run Kaniko
+exec proot \
+    -b /kaniko:/kaniko \
+    -b /home/gitlab-ce:/home/gitlab-ce \
+    -b /etc/mtab -b /etc/resolv.conf -b /etc/hosts \
+    /kaniko/executor --context /kaniko --dockerfile /kaniko/Dockerfile --destination $IMAGE_NAME
 
 # Keep the container running
-sleep infinity
+exec sleep infinity
